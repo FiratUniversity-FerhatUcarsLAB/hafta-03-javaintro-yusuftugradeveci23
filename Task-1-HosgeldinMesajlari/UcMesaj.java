45b5bb77a1df0b3bbfc436a70fcd011013cfdb0a
yusuftugradeveci23 <yusuftugradeveci23@gmail.com>
2025-10-20 18:05:47 +0300

    Delete Task-1-HosgeldinMesajlari/UcMesaj.java

--- Task-1-HosgeldinMesajlari/UcMesaj.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Java'ya Hoş Geldiniz");
-        System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
-        System.out.println("Programlama Eğlencelidir");
-    }
-}
-
