c614853600b2ade3dde8debbbdc3123c9db5e30e
yusuftugradeveci23 <yusuftugradeveci23@gmail.com>
2025-10-20 20:18:58 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,9 @@
+class IkiIslemSonucu {
+    public static void main(String[] args) {
+        double pay = 9.5 * 4.5 - 2.5 * 3;
+        double payda = 45.5 - 3.5;
+        double sonuc = pay / payda;
+
+        System.out.printf("Sonuç: %.4f", sonuc);
+    }
+}
