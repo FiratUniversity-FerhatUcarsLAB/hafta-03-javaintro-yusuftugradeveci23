0d58d09d3acb299958a6c15985dc798a7413e9ab
yusuftugradeveci23 <yusuftugradeveci23@gmail.com>
2025-10-20 20:27:15 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,7 @@
+public class IkiIslem {
+    public static void main(String[] args) {
+        int faktoriyel = 5 * 4 * 3 * 2 * 1;
+
+        System.out.println("5! = 4 × 3 × 2 × 1 × 5 = " + faktoriyel);
+    }
+}
