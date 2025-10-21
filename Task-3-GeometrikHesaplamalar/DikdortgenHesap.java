65515e94909463035d67ffa426714569e5588cf6
yusuftugradeveci23 <yusuftugradeveci23@gmail.com>
2025-10-21 21:23:31 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        double uzunKenar = 7.9;
+        double kisaKenar = 4.5;
+
+        double alan = uzunKenar * kisaKenar;
+        double cevre = 2 * (uzunKenar + kisaKenar);
+
+        System.out.println("Dikdörtgenin Alanı: " + alan);
+        System.out.println("Dikdörtgenin Çevresi: " + cevre);
+    }
+}
