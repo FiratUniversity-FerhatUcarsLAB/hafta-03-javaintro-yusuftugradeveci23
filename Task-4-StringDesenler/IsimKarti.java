7f497e9eb21ee78d5e27add6e372e66aa98fd0a2
yusuftugradeveci23 <yusuftugradeveci23@gmail.com>
2025-10-22 11:33:58 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class Main {
+ public static void main ( String [] args ) {
+         String ad = " Tugra ";
+         String soyad = " Deveci ";
+         String ogrNo = " 250541095 ";
+         String bolum = " Yazilim Muh .";
+
+         System . out . println (" =================== ") ;
+         System . out . println (" OGRENCI BILGILERI ") ;
+         System . out . println (" =================== ") ;
+        System . out . println ("Ad : " + ad ) ;
+        System . out . println (" Soyad : " + soyad ) ;
+         System . out . println ("No : " + ogrNo ) ;
+         System . out . println (" Bolum : " + bolum ) ;
+         System . out . println (" =================== ") ;
+         }
+ }
