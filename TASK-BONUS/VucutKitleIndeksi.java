bcc5d57696681c99f6487e311a0009e5cb5d5c6c
yusuftugradeveci23 <yusuftugradeveci23@gmail.com>
2025-10-23 20:21:22 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double kilo = 70; 
+        double boy = 1.75; 
+
+        double bmi = kilo / (boy * boy);
+
+        System.out.println("Vücut Kitle İndeksiniz: " + bmi);
+    }
+}
